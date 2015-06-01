bd16dbfeca7d8be14ab4345089d5cafc4821fc2f
Dana Scheider <dana.scheider@gmail.com>
2015-06-01 12:56:51 -0700

    Add database task helper from Tessitura

---
+++ config/database_task_helper.rb
@@ -0,0 +1,24 @@
+require 'yaml'
+
+module DatabaseTaskHelper
+  
+  def self.get_yaml(file)
+    yaml = (File.open(file, 'r+') {|file| YAML.load_file(file) }).to_h
+    yaml['defaults'] = yaml['defaults'].to_h
+
+    yaml['defaults'].keys.each do |key|
+      yaml['defaults'][(key.to_sym rescue key) || key] = yaml['defaults'].delete(key)
+    end
+
+    yaml
+  end
+
+  # Example:
+  #     hash = DatabaseTaskHelper.get_yaml('database.yml')['test']
+  #     DatabaseTaskHelper.get_string(hash, 'test')
+  #         # => "mysql2://root:rootpassword@localhost:3306/test"
+
+  def self.get_string(hash, env)
+    "#{hash[:adapter]}://#{hash[:username]}:#{hash[:password]}@#{hash[:host]}:#{hash[:port]}/#{env}"
+  end
+end
