cf6d4e10989243c46c678d7c696ee4e5a5c15e7f
Dana Scheider <dana.scheider@gmail.com>
2015-06-01 13:11:23 -0700

    Namespace DatabaseTaskHelper under DatabaseDestroyer

--- config/database_task_helper.rb
+++ config/database_task_helper.rb
@@ -1,24 +1,25 @@
 require 'yaml'
 
-module DatabaseTaskHelper
-  
-  def self.get_yaml(file)
-    yaml = (File.open(file, 'r+') {|file| YAML.load_file(file) }).to_h
-    yaml['defaults'] = yaml['defaults'].to_h
+class DatabaseDestroyer < Sinatra::Base
+  module DatabaseTaskHelper
+    def self.get_yaml(file)
+      yaml = (File.open(file, 'r+') {|file| YAML.load_file(file) }).to_h
+      yaml['defaults'] = yaml['defaults'].to_h
 
-    yaml['defaults'].keys.each do |key|
-      yaml['defaults'][(key.to_sym rescue key) || key] = yaml['defaults'].delete(key)
+      yaml['defaults'].keys.each do |key|
+        yaml['defaults'][(key.to_sym rescue key) || key] = yaml['defaults'].delete(key)
+      end
+
+      yaml
     end
 
-    yaml
-  end
+    # Example:
+    #     hash = DatabaseTaskHelper.get_yaml('database.yml')['test']
+    #     DatabaseTaskHelper.get_string(hash, 'test')
+    #         # => "mysql2://root:rootpassword@localhost:3306/test"
 
-  # Example:
-  #     hash = DatabaseTaskHelper.get_yaml('database.yml')['test']
-  #     DatabaseTaskHelper.get_string(hash, 'test')
-  #         # => "mysql2://root:rootpassword@localhost:3306/test"
-
-  def self.get_string(hash, env)
-    "#{hash[:adapter]}://#{hash[:username]}:#{hash[:password]}@#{hash[:host]}:#{hash[:port]}/#{env}"
+    def self.get_string(hash, env)
+      "#{hash[:adapter]}://#{hash[:username]}:#{hash[:password]}@#{hash[:host]}:#{hash[:port]}/#{env}"
+    end
   end
 end
